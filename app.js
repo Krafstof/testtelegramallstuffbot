32178bdaca4c3f17f73b773c1d7ba6297979f0a5
andkrav <andrey.kravtsov@graebert.com>
2023-04-19 00:26:46 +0300

    adfgs

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let tg = window.Telegram.WebApp;
 var BackButton = tg.BackButton;
+BackButton.show();
 
 BackButton.addEventListener('click', () => {
  tg.showAlert(`Добро пожаловать, @${WebApp.WebAppUser.username}.`);
-  });
+});
