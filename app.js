df76d3c4aca5e3cead7b4dc9379d682bb1f63ff9
andkrav <andrey.kravtsov@graebert.com>
2023-04-19 00:28:19 +0300

    asdfadsfadfs

--- app.js
+++ app.js
@@ -5,3 +5,4 @@ BackButton.show();
 BackButton.addEventListener('click', () => {
  tg.showAlert(`Добро пожаловать, @${WebApp.WebAppUser.username}.`);
 });
+//<iframe src="iframeInner.html" frameborder="0" style="overflow:hidden;overflow-x:hidden;overflow-y:hidden;height:100%;width:100%;position:absolute;top:0px;left:0px;right:0px;bottom:0px" height="100%" width="100%"></iframe>
